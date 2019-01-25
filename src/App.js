7a1746d242da539dbea8901279f76c9382ec29df
MCVNick <mcvnick@gmail.com>
2019-01-25 09:54:06 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { HashRouter as Router } from 'react-router-dom'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
